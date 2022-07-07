90e94f5ec5e2830eb778e95baf0da96b1927be19
Juan-Lee Pang <jpang@microsoft.com>
2022-07-07 07:53:59 -0700

    Larger terminal

--- config/nvim/after/plugin/toggleterm.lua
+++ config/nvim/after/plugin/toggleterm.lua
@@ -1,7 +1,14 @@
-require("toggleterm").setup{
+require("toggleterm").setup {
     open_mapping = [[<leader>\]],
     insert_mappings = false,
     direction = "horizontal",
+    size = function(term)
+        if term.direction == "horizontal" then
+            return vim.o.lines * 0.5
+        elseif term.direction == "vertical" then
+            return vim.o.columns * 0.5
+        end
+    end,
     float_opts = {
         border = "curved",
     }
