d8c2121593780d059dc0af52be1557c068c17f93
Juan-Lee Pang <jpang@microsoft.com>
2022-10-16 09:54:23 -0700

    stop using deprecated api in cmp-nvim-lsp

--- config/nvim/after/plugin/nvim-lspconfig.lua
+++ config/nvim/after/plugin/nvim-lspconfig.lua
@@ -29,7 +29,7 @@ local on_attach = function(client, bufnr)
 end
 
 local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 lspconfig.gopls.setup {
     capabilities = capabilities,
