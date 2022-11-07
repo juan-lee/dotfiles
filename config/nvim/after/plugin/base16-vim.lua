dda6ecd1575fc9e6fb55b748deabfdf787eefbe2
Juan-Lee Pang <jpang@microsoft.com>
2022-11-06 19:59:23 -0800

    Switch things up a bit

--- config/nvim/after/plugin/base16-vim.lua
+++ config/nvim/after/plugin/base16-vim.lua
@@ -1,8 +1,9 @@
+local fn = vim.fn
 local cmd = vim.cmd
-local g = vim.g
+local set_theme_path = "$HOME/.config/tinted-theming/set_theme.lua"
+local is_set_theme_file_readable = fn.filereadable(fn.expand(set_theme_path)) == 1 and true or false
 
-local base16_project_theme = os.getenv('BASE16_THEME')
-if base16_project_theme and g.colors_name ~= 'base16-'..base16_project_theme then
-  cmd('let base16colorspace=256')
-  cmd('colorscheme base16-'..base16_project_theme)
+if is_set_theme_file_readable then
+  cmd("let base16colorspace=256")
+  cmd("source " .. set_theme_path)
 end
